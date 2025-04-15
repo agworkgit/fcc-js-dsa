65224bcfbf2b24b1e77d5caa69443cafffa165ce
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-04-15 11:24:20 +0100

    notes and code update

---
+++ 01_comments/script.js
@@ -0,0 +1,2 @@
+// This is an in-line comment with CMD+/ on Mac
+/* This is a multi-line comment with CMD+SHIFT+A on Mac*/
---
+++ 12_subtract_two_nums/script.js
@@ -0,0 +1,18 @@
+/* 
+We can also subtract one number from another.
+
+JavaScript uses the - symbol for subtraction.
+
+Example
+
+const myVar = 12 - 6;
+myVar would have the value 6.
+
+Change the 0 so the difference is 12.
+*/
+
+// const difference = 45 - 0;
+
+const difference = 45 - 33;
+
+console.log(difference);
